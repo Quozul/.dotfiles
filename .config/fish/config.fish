d87e8471b7eba968beae7641bca50f0614398c4e
Erwan <erwan.legloannec@outlook.com>
2023-05-30 11:39:08 +0200

    Add Sway config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
 set fish_greeting
 
 starship init fish | source
+
+#nvm use 18
+
+# pnpm
+set -gx PNPM_HOME "/home/erwan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm endrtx activate fish | source
+rtx activate fish | source
