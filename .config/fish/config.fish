7126c7d143e723339ae902ea007714e9b4146d32
Erwan <erwan.legloannec@outlook.com>
2023-05-30 15:17:53 +0200

    Update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,5 +9,9 @@ set -gx PNPM_HOME "/home/erwan/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+
 # pnpm endrtx activate fish | source
+
 rtx activate fish | source
+
+# pnpm end
